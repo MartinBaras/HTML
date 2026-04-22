1acad0a4c907fffffacf2f0d5550d64b577b5f4b
Martin <barasmartinezequiel@gmail.com>
2026-04-21 23:38:53 -0300

    Add files via upload

--- tarea/tarea.js
+++ tarea/tarea.js
@@ -1,22 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Variables/Constantes
     const input_box = document.querySelector("#input-box");
     const editable_button = document.querySelector("#editable-button");
     const submit_button = document.querySelector("#submit-button");
     const colores = document.querySelectorAll(".caja-color");
-    // Funciones
+
     function CambiarTexto() {
         editable_button.textContent = input_box.value;
-    };
+    }
 
     function CambiarFondo() {
         colores.forEach(color => {
             color.addEventListener("click", () => {
-                color.classList.toggle("bg-selection");
+                colores.forEach(c => c.classList.remove("bg-selection"));
+                color.classList.add("bg-selection");
             });
         });
+    }
 
-    };
-    // EventsListeners
     submit_button.addEventListener("click", CambiarTexto);
+    CambiarFondo();
 });
