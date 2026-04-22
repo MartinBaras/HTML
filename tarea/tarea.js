4826aa4f4ae95aee181560be08f1f6aa9352d89c
Martin <barasmartinezequiel@gmail.com>
2026-04-22 12:56:59 -0300

    Add files via upload

--- tarea/tarea.js
+++ tarea/tarea.js
@@ -3,20 +3,38 @@ document.addEventListener("DOMContentLoaded", () => {
     const editable_button = document.querySelector("#editable-button");
     const submit_button = document.querySelector("#submit-button");
     const colores = document.querySelectorAll(".caja-color");
+    let color_viejo;
+    let color_nuevo;
 
     function CambiarTexto() {
         editable_button.textContent = input_box.value;
     }
 
-    function CambiarFondo() {
+    function SeleccionarColor() {
         colores.forEach(color => {
             color.addEventListener("click", () => {
-                colores.forEach(c => c.classList.remove("bg-selection"));
-                color.classList.add("bg-selection");
+                colores.forEach(c => c.classList.remove("border-selection"));
+                color.classList.add("border-selection");
             });
         });
     }
+    function CambiarColor() {
+        color_viejo=editable_button.classList[2]
+        colores.forEach(color => {
+            if (color.classList.contains("border-selection")){
+                color_nuevo=color.classList[1]
+                if (color_viejo!==color_nuevo){
+                    editable_button.classList.remove(color_viejo);
+                    editable_button.classList.add(color_nuevo);
+                };
+            };
+        });
+    }
 
-    submit_button.addEventListener("click", CambiarTexto);
-    CambiarFondo();
+    submit_button.addEventListener("click",()=>{
+        CambiarTexto();
+        CambiarColor();
+    });
+
+    SeleccionarColor();
 });
